5bb87b158e82aaa64f04b51b37df1dae79879a88
harshvardhan singh <harshvardhansingh458@gmail.com>
2022-06-03 18:34:38 +0530

    first commit

---
+++ src/components/Interest.js
@@ -0,0 +1,9 @@
+const Interest = () => {
+    return (
+        <div>
+            <h3>Interest</h3>
+        </div>
+    )
+}
+
+export default Interest
---
+++ src/components/ProfessionalInfo.js
@@ -0,0 +1,9 @@
+const ProfessionalInfo = () => {
+    return (
+        <div>
+            <h1>Professional Information</h1>
+        </div>
+    )
+}
+
+export default ProfessionalInfo
